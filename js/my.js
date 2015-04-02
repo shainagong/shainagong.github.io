069d8e8c1caaea26b6ea28638e22bea5ae6d7736
Shaina Going <shainagong@gmail.com>
2015-04-02 02:55:55 -0400

    Minor changes

--- js/my.js
+++ js/my.js
@@ -18,23 +18,3 @@ $('#portfolio_link').on('click', function(e) {
 	$('#banner').removeAttr('padding');
 	$('#banner').load("portfolio.html");
 });
-
-/*
-
-	$('#banner').html("");
-	$('section#banner').removeAttr('id');
-	$('section').attr('#banner-portfolio');
-	$('section#banner-portfolio').load("portfolio.html");
-
-		{'background-attachment': 'scroll, scroll, fixed',
-		'background-color': '#ffffff',
-		'background-image': 'url("images/light-bl.svg"), url("images/light-br.svg"), url("../images/banner.jpg")',
-		'background-position': 'top center, top center, top center',
-		'background-repeat': 'no-repeat, no-repeat, no-repeat',
-		'background-size': '50em, 50em, auto',
-		'color': 'white',
-		'cursor': 'default',
-		'padding': '5em 0em 5em 0em',
-		'margin': '3em 3em 0em 3em',
-		'text-align': 'center'}
-		*/
